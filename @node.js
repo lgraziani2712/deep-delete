ffa658ad713ef72a66b5ab286541ac4c3fd15335
Luciano Graziani <lgraziani2712@gmail.com>
2018-10-21 15:11:33 -0300

    Fix #2 Add Node compatibility

---
+++ @node.js
@@ -0,0 +1,25 @@
+/**
+ * Removes every matched key recursively.
+ *
+ * @param {string|Array<string>} filters The key(s) to delete.
+ * @param {any} data The data to be processed.
+ * @returns {any} The cleaned data.
+ */
+module.exports = function deepDelete(filters, data) {
+  if (Array.isArray(data)) {
+    return data.map(element => deepDelete(filters, element));
+  }
+  if (!data || typeof data !== 'object') {
+    return data;
+  }
+  const hasMultipleFilters = Array.isArray(filters);
+
+  return Object.keys(data).reduce((partial, key) => {
+    if (hasMultipleFilters ? filters.includes(key) : key === filters) {
+      return partial;
+    }
+    partial[key] = deepDelete(filters, data[key]);
+
+    return partial;
+  }, {});
+};
